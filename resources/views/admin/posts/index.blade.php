485e7cd6e259f9b0da8cf708f2a76affbdeab90c
Ermal Arapi <ermalarapi@yahoo.com>
2018-06-11 12:07:12 +0200

    able to create posts with categoryes

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -28,7 +28,7 @@
         <td>{{$post->id}}</td>  
         <td><img height="60" width="100" src="{{$post->photo ? $post->photo->file : 'no photo'}}"> </td>          
         <td>{{$post->user->name}}</td>                
-        <td>{{$post->category_id}}</td>  
+        <td>{{$post->category ? $post->category->name : 'uncategorized'}}</td>  
         <td>{{$post->title}}</td>  
         <td>{{$post->body}}</td>  
         <td>{{$post->created_at}}</td>  
